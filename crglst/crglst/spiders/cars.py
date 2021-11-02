c42608b851ceb10c54b51700096fe60a7afa8869
EC2 Default User <ec2-user@ip-172-31-92-177.ec2.internal>
2021-11-02 19:44:24 +0000

    changed collection logick

--- crglst/crglst/spiders/cars.py
+++ crglst/crglst/spiders/cars.py
@@ -17,26 +17,26 @@ class ExploitSpider(scrapy.Spider):
     
     def __init__(self):
         page_number = 120
-        urls =  ['https://raleigh.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1',
-                    'https://NewYork.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1',
-                    'https://LosAngeles.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1',
-                    'https://LosAngeles.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1',
-                    'https://Chicago.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1',
-                    'https://Houston.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1',
-                    'https://Phoenix.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1',
-                    'https://Philadelphia.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1',
-                    'https://SanAntonio.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1',
-                    'https://SanDiego.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1',
-                    'https://Dallas.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1',
-                    'https://Austin.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1',
-                    'https://Jacksonville.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1',
-                    'https://Columbus.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1',
-                    'https://Charlotte.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1',
-                    'https://SanFrancisco.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1',
-                    'https://Indianapolis.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1',
-                    'https://Seattle.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1',
-                    'https://Denver.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1'
-                    ]
+        urls =  ['https://raleigh.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1']#,
+                    # 'https://NewYork.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1',
+                    # 'https://LosAngeles.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1',
+                    # 'https://LosAngeles.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1',
+                    # 'https://Chicago.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1',
+                    # 'https://Houston.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1',
+                    # 'https://Phoenix.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1',
+                    # 'https://Philadelphia.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1',
+                    # 'https://SanAntonio.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1',
+                    # 'https://SanDiego.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1',
+                    # 'https://Dallas.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1',
+                    # 'https://Austin.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1',
+                    # 'https://Jacksonville.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1',
+                    # 'https://Columbus.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1',
+                    # 'https://Charlotte.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1',
+                    # 'https://SanFrancisco.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1',
+                    # 'https://Indianapolis.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1',
+                    # 'https://Seattle.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1',
+                    # 'https://Denver.craigslist.org/d/cars-trucks-by-owner/search/cto?hasPic=1'
+                    # ]
         for url in urls:
             self.start_urls.append(url)
             for i in range(1,25):
@@ -48,7 +48,7 @@ class ExploitSpider(scrapy.Spider):
     #         yield Request(url, callback=self.parse)
 
     def parse(self, response):
-        #items = CrglstItem()
+        items = CrglstItem()
         car_year_name = response.css('.hdrlnk::text').extract()
         car_price = response.css('.result-meta .result-price').css('::text').extract()
         # d_car_urls = response.css('img ::attr(src)').getall()
@@ -60,19 +60,21 @@ class ExploitSpider(scrapy.Spider):
         # }
         #car_total = response.css('.totalcount::text').extract()
         #car_info = response.css('.result-meta .result-price , .hdrlnk').css('::text').extract()
+        # for i in range(len(car_price)):
+        #     append_csv_file(car_year_name[i],car_price[i])
         for i in range(len(car_price)):
-            append_csv_file(car_year_name[i],car_price[i])
-        #items['car_year_name'] = car_year_name
-        #items['car_price'] = car_price
+            items['car_year_name'] = car_year_name[i]
+            items['car_price'] = car_price[i]
+            yield items
         #print('{} - {}'. format(car_year_name,car_price))
-        #yield items
+        
         
         # for url in self.start_urls:
         #     next_page = url + '?s=' + str(ExploitSpider.page_number)
         #     if ExploitSpider.page_number < 3000:
         #         ExploitSpider.page_number = ExploitSpider.page_number + 120
         #         yield response.follow(next_page, callback = self.parse,dont_filter=True)
-          
+
        
 def append_csv_file(car_year_name,car_price):
     line = f"{car_year_name} - {car_price}\n"
